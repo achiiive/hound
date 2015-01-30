76e33f0b4f704e4aa35f6479b6bcd58e623fae98
srt32 <simon.taranto@gmail.com>
2015-01-30 09:50:49 -0800

    Remove if

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -4,11 +4,7 @@ class Owner < ActiveRecord::Base
   def self.upsert(github_id:, github_name:)
     owner = find_or_initialize_by(github_id: github_id)
     owner.github_name = github_name
-
-    if owner.changed?
-      owner.save
-    end
-
+    owner.save
     owner
   end
 end
